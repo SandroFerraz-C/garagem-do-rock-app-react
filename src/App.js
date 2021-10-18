2b130c2ff2a6d0be6f689d6fa85d0cea3db4a65f
Sandro Ferraz <sergiosandro86@gmail.com>
2021-10-18 09:23:58 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React from 'react';
+import Main from './components/Main'
+import Acdc from './components/bands/acdc/Acdc'
+import Aerosmith from './components/bands/Aerosmith'
+import Angra from './components/bands/Angra'
+import ButtonHome from './components/butoon_home/ButtonHome'
+import HistoryAcdc from './components/bands/acdc/HistoryAcdc'
+import AcdcAustraliano from './components/bands/acdc/AcdcAustraliano'
+
+
+import {Switch, Route} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       
-    </div>
+      <Switch>
+        <Route path='/' exact component={Main} />
+        <Route path='/acdc' component={Acdc} />
+        <Route path='/aerosmith' component={Aerosmith} />
+        <Route path='/angra' component={Angra} />
+        <Route path='/' exact component={ButtonHome} />
+        <Route path='/historyAcdc' exact component={HistoryAcdc} />
+        <Route path='/australiano' exact component={AcdcAustraliano} />
+      </Switch>
+      
+    </>
   );
 }
 
